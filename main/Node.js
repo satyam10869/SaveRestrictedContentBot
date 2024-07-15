55aa1de165a250b7abd8eb64d1dbb5178725a37b
satyam10869 <133392730+satyam10869@users.noreply.github.com>
2024-07-15 10:39:39 +0530

    Update and rename __init__.py to Node.js

---
+++ main/Node.js
@@ -0,0 +1,7 @@
+// Importing necessary modules/packages in JavaScript
+const { TelegramClient } = require('telegram');
+const { Client } = require('pyrogram');
+const { config } = require('decouple');
+const logging = require('logging');
+const sys = require('sys');
+const time = require('time');
